c81ce9f6fc78d9003a2bfe0d078bc6965212fca6
George Baugh <george@troglodyne.net>
2025-10-02 15:13:25 -0500

    Don't forget starman dep

--- bin/deps2hash.sh
+++ bin/deps2hash.sh
@@ -2,4 +2,5 @@
 # Because I am LAZY
 echo "PREREQ_PM => {"
 git grep -P 'use \S+;' | sed -e 's/^.*:use //g;s/[(|)|;]//g' | grep -vP 'strict|warnings' | sort | uniq | sed -e 's/\(.*\)/"\1"/;s/$/ => "0",/'
+echo 'Starman => "0"'
 echo "}"
